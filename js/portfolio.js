9190d306d66513b341c1c4b220fe34c00206ba62
Mars <marshallmiller92@gmail.com>
2016-03-08 18:16:49 -0800

    initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,44 @@
+var tabContent = [];
+
+function Portfolio (opts) {
+  this.project = opts.project;
+  this.projectUrl = opts.projectUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Portfolio.prototype.toHtml = function() {
+  var $newPortfolio = $('projects.template').clone();
+  $newPortfolio.removeClass('template');
+  if (!this.publishedOn) {
+    $newPortfolio.addClass('draft');
+  }
+  $newPortfolio.attr('data-category', this.category);
+  // TODO: Use jQuery to also add the project name as a data-attribute of the newly cloned projects.
+  //       Doing so will allow us to use selectors to target articles, based on who wrote them.
+  $newPortfolio.attr('data-project', this.project);
+
+  $newPortfolio.find('.byline a').html(this.project);
+  $newPortfolio.find('.byline a').attr('href', this.projectUrl);
+  $newPortfolio.find('h1:first').html(this.title);
+  $newPortfolio.find('.projects-body').html(this.body);
+  $newPortfolio.find('time[pubdate]').attr('datetime', this.publishedOn)
+  $newPortfolio.find('time[pubdate]').attr('title', this.publishedOn)
+  $newPortfolio.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newPortfolio.append('<hr>');
+  return $newPortfolio;
+}
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  tabContent.push(new Portfolio(ele));
+})
+
+tabContent.forEach(function(a){
+  $('#projects').append(a.toHtml())
+});
---
+++ js/tabSlide.js
@@ -0,0 +1,32 @@
+var tabSlide = {};
+
+tabSlide.populateTab = function() {
+  $('projects').each(function() {
+    if (!$(this).hasClass('template')) {
+
+      var val = $(this).find('address a').text();
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('#project-filter').append(optionTag);
+
+      val = $(this).attr('data-category');
+      optionTag = '<option value="' + val + '">' + val + '</option>';
+      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+tabSlide.handleMainNav = function() {
+  $('.main-nav').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
+$(document).ready(function() {
+  tabSlide.populateTab();
+  tabSlide.handleMainNav();
+});

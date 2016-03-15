effc64e2238aa874650f6abcc59a1b288bc5529b
Mars <marshallmiller92@gmail.com>
2016-03-15 16:35:40 -0700

    divided FetchAll into seperate functions to handle different objects

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,70 +1,100 @@
-
-function Portfolio (opts) {
-  this.project = opts.project;
-  this.projectUrl = opts.projectUrl;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
-}
-
-Portfolio.all = [];
-
-Portfolio.prototype.toHtml = function() {
-  var source = $('#projects-template').html()
-  var template = Handlebars.compile(source);
-
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-
-
-  return template(this);
-}
-
-Portfolio.fetchAll = function() {
-  if (localStorage.projectData) {
-    Portfolio.loadAll(JSON.parse(localStorage.projectData));
-    tabSlide.initIndexPage();
-  } else {
-    $.getJSON('/data/JSON.json', function(projectData){
-      console.log(projectData);
-      Portfolio.loadAll(projectData);
-      localStorage.projectData = JSON.stringify(projectData);
-      tabSlide.initIndexPage();
-    })
+(function(module) {
+  function Portfolio (opts) {
+    this.project = opts.project;
+    this.projectUrl = opts.projectUrl;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
   }
-}
 
-// Portfolio.fetchAll = function() {
-//   if (localStorage.projectData) {
-//     $.ajax({
-//       type: 'HEAD',
-//       url: '/data/JSON.json',
-//       success: function(data, message, xhr) {
-//         console.log(xhr);
-//         var eTag = xhr.getResponseHeader('eTag');
-//         if (!localStorage.eTag || eTag !== localStorage.eTag) {
-//           localStorage.eTag = eTag;
-//           Portfolio.getAll();
-//         } else {
-//           Portfolio.loadAll(JSON.parse(localStorage.projectData));
-//           tabSlide.initIndexPage();
-//         }
-//       }
-//     });
-//   } else {
-//     Portfolio.getAll();
-//   }
-// };
+  Portfolio.projArray = [];
+  Portfolio.schoolArray = [];
+  Portfolio.workArray = [];
 
-Portfolio.loadAll = function(projectData) {
-  projectData.sort(function(a, b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
+  Portfolio.prototype.toHtml = function() {
+    var source = $('#projects-template').html()
+    var template = Handlebars.compile(source);
 
-  projectData.forEach(function(ele) {
-    Portfolio.all.push(new Portfolio(ele));
-  });
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  console.log('test3')
-};
+
+    return template(this);
+  }
+
+
+  Portfolio.fetchProjects = function(callback) {
+    if (localStorage.projectData) {
+      Portfolio.loadProjects(JSON.parse(localStorage.projectData));
+      callback();
+    } else {
+      $.getJSON('/data/JSON.json', function(projectData){
+        console.log(projectData);
+        Portfolio.loadProjects(projectData);
+        localStorage.projectData = JSON.stringify(projectData);
+        callback();
+      })
+    }
+  }
+
+  Portfolio.fetchSchool = function(callback) {
+    if (localStorage.schoolData) {
+      Portfolio.loadSchool(JSON.parse(localStorage.schoolData));
+      callback();
+    } else {
+        $.getJSON('/data/school.json', function(schoolData){
+        console.log(schoolData);
+        Portfolio.loadSchool(schoolData);
+        localStorage.schoolData = JSON.stringify(schoolData);
+        callback();
+      })
+    }
+  }
+
+  Portfolio.fetchWork = function(callback) {
+    if (localStorage.workData) {
+      Portfolio.loadWork(JSON.parse(localStorage.workData));
+      callback();
+    } else {
+      $.getJSON('/data/work.json', function(workData){
+        console.log(workData);
+        Portfolio.loadWork(workData);
+        localStorage.workData = JSON.stringify(workData);
+        callback();
+      })
+    }
+  }
+
+  Portfolio.loadProjects = function(data) {
+    data.sort(function(a, b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    data.forEach(function(ele) {
+      Portfolio.projArray.push(new Portfolio(ele));
+    });
+  };
+
+  Portfolio.loadSchool = function(data) {
+    data.sort(function(a, b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    data.forEach(function(ele) {
+      Portfolio.schoolArray.push(new Portfolio(ele));
+    });
+  };
+
+  Portfolio.loadWork = function(data) {
+    data.sort(function(a, b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    data.forEach(function(ele) {
+      Portfolio.workArray.push(new Portfolio(ele));
+    });
+  };
+
+  module.Portfolio = Portfolio;
+})(window)

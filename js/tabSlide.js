eb36fc71b170f04bfda368fea8c513920928cf61
Mars <marshallmiller92@gmail.com>
2016-03-15 12:14:10 -0700

    set up IIFE

--- js/tabSlide.js
+++ js/tabSlide.js
@@ -1,39 +1,46 @@
-var tabSlide = {};
 
-tabSlide.populateTab = function() {
-  $('projects').each(function() {
-    if (!$(this).hasClass('template')) {
+(function(module) {
 
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#project-filter').append(optionTag);
 
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
+  var tabSlide = {};
+
+  tabSlide.populateTab = function() {
+    $('projects').each(function() {
+      if (!$(this).hasClass('template')) {
+
+        var val = $(this).find('address a').text();
+        var optionTag = '<option value="' + val + '">' + val + '</option>';
+        $('#project-filter').append(optionTag);
+
+        val = $(this).attr('data-category');
+        optionTag = '<option value="' + val + '">' + val + '</option>';
+        if ($('#category-filter option[value="' + val + '"]').length === 0) {
+          $('#category-filter').append(optionTag);
+        }
       }
-    }
-  });
-};
+    });
+  };
 
-tabSlide.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
+  tabSlide.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
 
-  $('.main-nav .tab:first').click();
-};
+    $('.main-nav .tab:first').click();
+  };
 
 
-tabSlide.initIndexPage = function() {
-  Portfolio.all.forEach(function(a){
-    $('#projects').append(a.toHtml())
-    $('#work').append(a.toHtml())
-    $('#school').append(a.toHtml())
-  });
+  tabSlide.initIndexPage = function() {
+    Portfolio.all.forEach(function(a){
+      $('#projects').append(a.toHtml())
+      $('#work').append(a.toHtml())
+      $('#school').append(a.toHtml())
+    });
 
-  tabSlide.populateTab();
-  tabSlide.handleMainNav();
-};
+    tabSlide.populateTab();
+    tabSlide.handleMainNav();
+  };
+  
+  module.tabSlide = tabSlide;
+})(window);

4a358c12453b9eaa4988864a4c98af642a963308
Mars <marshallmiller92@gmail.com>
2016-03-22 15:25:10 -0700

    reworked one route

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
-page('/about', aboutController.index);
 page('/project', projectController.index);
 page('/work', workController.index);
 page('/school', schoolController.index);
+page('/', aboutController.index);
 
 page();

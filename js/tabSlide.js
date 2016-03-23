857d4708c075b7a295e70de53653d197c0f14412
Mars <marshallmiller92@gmail.com>
2016-03-23 13:08:02 -0700

    refactored tabslide

--- js/tabSlide.js
+++ js/tabSlide.js
@@ -1,61 +1,21 @@
 (function(module) {
   var tabSlide = {};
 
-  tabSlide.populateTab = function() {
-    $('projects').each(function() {
-      if (!$(this).hasClass('template')) {
-
-        var val = $(this).find('address a').text();
-        var optionTag = '<option value="' + val + '">' + val + '</option>';
-        $('#project-filter').append(optionTag);
-
-        val = $(this).attr('data-category');
-        optionTag = '<option value="' + val + '">' + val + '</option>';
-        if ($('#category-filter option[value="' + val + '"]').length === 0) {
-          $('#category-filter').append(optionTag);
-        }
-      }
-    });
-  };
-
   function makeInitAll(dataArray, id) {
-    Portfolio[dataArray] = forEach(function(a){
-      $(id).append(a.toHtml())
-    });
+    return function() {
+      Portfolio[dataArray].forEach(function(a){
+        $(id).append(a.toHtml())
+      });
 
-    tabSlide.populateTab();
-    $(document).ready(function(){
-      $('.bxslider').bxSlider();
-    });
+      $(document).ready(function(){
+        $('.bxslider').bxSlider();
+      });
+    }
   }
 
   tabSlide.initSchool = makeInitAll('schoolArray', '#school');
   tabSlide.initProjects = makeInitAll('projArray', '#projects');
   tabSlide.initWork = makeInitAll('workArray', '#work');
 
-
-//
-//   tabSlide.initProjects = function() {
-//     Portfolio.projArray.forEach(function(a){
-//       $('#projects').append(a.toHtml())
-//     });
-//     tabSlide.populateTab();
-//
-//     $(document).ready(function(){
-//   $('.bxslider').bxSlider();
-// });
-//   }
-//
-//   tabSlide.initSchool = function() {
-//     Portfolio.schoolArray.forEach(function(a){
-//       $('#school').append(a.toHtml())
-//     });
-//   }
-//
-//   tabSlide.initWork = function() {
-//     Portfolio.workArray.forEach(function(a){
-//       $('#work').append(a.toHtml())
-//     });
-//   }
   module.tabSlide = tabSlide;
 })(window);
